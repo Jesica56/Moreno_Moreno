3db56e1f5dd458a4b6fc2e9699b6755c3ef8ee4b
Jesica56 <martinezjesicanoelia56@gmail.com>
2024-01-04 21:38:56 -0300

    cambios en los modales

--- js/proventistas.js
+++ js/proventistas.js
@@ -1,49 +1,48 @@
 document.addEventListener("DOMContentLoaded", function() {
+    const openModal = (targetId) => {
+        const modal = document.getElementById(targetId);
+        if (modal) {
+            modal.style.display = "block";
+        }
+    };
+
+    const closeModal = (button) => {
+        const modal = button.closest(".modal");
+        if (modal) {
+            modal.style.display = "none";
+        }
+    };
+
     const modalLinks = document.querySelectorAll(".prev-lista a");
-    
     modalLinks.forEach(function(link) {
         link.addEventListener("click", function(e) {
             e.preventDefault();
             const targetId = this.getAttribute("href").replace("#", "");
-            const modal = document.getElementById(targetId);
-            
-            if (modal) {
-                modal.style.display = "block";
-            }
+            openModal(targetId);
         });
     });
 
     const closeButtons = document.querySelectorAll(".close-button");
-
     closeButtons.forEach(function(button) {
         button.addEventListener("click", function() {
-            const modal = this.parentNode.parentNode;
-            modal.style.display = "none";
+            closeModal(this);
+        });
+    });
+
+    // Agregar funcionalidad para otra ventana modal
+    const modalLinks2 = document.querySelectorAll(".prev-list-mod a");
+    modalLinks2.forEach(function(link) {
+        link.addEventListener("click", function(e) {
+            e.preventDefault();
+            const targetId = this.getAttribute("href").replace("#", "");
+            openModal(targetId);
+        });
+    });
+
+    const closeButtons2 = document.querySelectorAll(".close-button-2");
+    closeButtons2.forEach(function(button) {
+        button.addEventListener("click", function() {
+            closeModal(this);
         });
     });
 });
-
-
-   // Agregar funcionalidad para otra ventana modal
-   const modalLinks2 = document.querySelectorAll(".prev-list-mod a");
-
-   modalLinks2.forEach(function(link) {
-       link.addEventListener("click", function(e) {
-           e.preventDefault();
-           const targetId = this.getAttribute("href").replace("#", "");
-           const modal = document.getElementById(targetId);
-           
-           if (modal) {
-               modal.style.display = "block";
-           }
-       });
-   });
-
-   const closeButtons2 = document.querySelectorAll(".close-button-2");
-
-   closeButtons2.forEach(function(button) {
-       button.addEventListener("click", function() {
-           const modal = this.parentNode.parentNode;
-           modal.style.display = "none";
-       });
-   });
