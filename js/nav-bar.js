cf2ac9e83f86458e6d1e2a03021bd0e7ddf1293d
Jesica56 <martinezjesicanoelia56@gmail.com>
2024-01-30 18:55:42 -0300

    Cambios
    cambios en las imagenes y en el navbar

--- js/nav-bar.js
+++ js/nav-bar.js
@@ -1,7 +1,31 @@
-$('.navTrigger').click(function () {
-  $(this).toggleClass('active');
-  console.log("Clicked menu");
-  $("#mainListDiv").toggleClass("show_list");
-  $("#mainListDiv").fadeIn();
-});
+$(document).ready(function () {
+  // Manejar el clic en el botón de la hamburguesa
+  $('.navTrigger').click(function () {
+    $(this).toggleClass('active');
+    console.log("Clicked menu");
+    toggleMenu();
+  });
 
+  // Manejar el clic en los elementos <li>
+  $("#mainListDiv ul.navlinks li").click(function () {
+    // Cierra el menú
+    $(".navTrigger").removeClass("active");
+    hideMenu();
+  });
+
+  // Función para alternar la visibilidad del menú
+  function toggleMenu() {
+    $("#mainListDiv").toggleClass("show_list");
+    if ($("#mainListDiv").hasClass("show_list")) {
+      $("#mainListDiv").fadeIn();
+    } else {
+      hideMenu();
+    }
+  }
+
+  // Función para ocultar el menú
+  function hideMenu() {
+    $("#mainListDiv").fadeOut();
+    $("#mainListDiv").removeClass("show_list");
+  }
+});
